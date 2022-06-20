94a87d98b4773f666a45406d015a29d9ec636180
Hayden <hayden@tpcinternet.com>
2022-06-21 07:04:22 +0900

    style: Add styles on upload page

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div>
+    <div className='px-4 py-16'>
       <div>
         <div>
-          <label>
+          <label className='w-full  h-48 flex items-center justify-center border-2 border-dashed border-gray-300 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500 cursor-pointer transition-colors'>
             <svg
               className="h-12 w-12"
               stroke="currentColor"
@@ -20,30 +20,47 @@ const Upload: NextPage = () => {
                 strokeLinejoin="round"
               />
             </svg>
-
-            <input type="file" />
+            <input type="file" className='hidden' />
           </label>
         </div>
       </div>
-      <div>
-        <label>Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <div className='my-5'>
+        <label
+          htmlFor='price'
+          className='mb-1 text-sm font-medium text-gray-700'
+        >
+          Price
+        </label>
+        <div className='relative rounded-md flex items-center shadow-sm'>
+          <div className='absolute left-0 pl-3 flex items-center justify-center'>
+            <span className='text-gray-500 text-sm'>$</span>
           </div>
-          <input type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input
+            type="text"
+            placeholder="0.00"
+            className='appearance-none w-full pl-7 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
+          />
+          <div className='absolute right-0 pr-3 flex items-center pointer-events-none'>
+            <span className='text-gray-500'>USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label>Description</label>
+        <label className='mb-1 text-sm font-medium text-gray-700'>
+          Description
+        </label>
         <div>
-          <textarea rows={4} />
+          <textarea
+            rows={4}
+            className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500'
+          />
         </div>
       </div>
-      <button>Upload product</button>
+      <button
+        className='mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'
+      >
+        Upload product
+      </button>
     </div>
   );
 };
