8f31203a5094613a2ab4b5987fa07053a5b30b80
Hayden <hayden@tpcinternet.com>
2022-06-21 14:47:46 +0900

    refactor: refact clsName fn

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-
-function setClassName(...classnames: string[]) {
-  return classnames.join(' ')
-}
+import { setClassName } from '../libs/utils';
 
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
