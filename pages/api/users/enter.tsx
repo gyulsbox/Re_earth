ba30111815a8095fc4585f5e823c47fadc3a473e
Hayden <hayden@tpcinternet.com>
2022-06-24 01:25:34 +0900

    feat: refactoring enter api

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,36 @@
-import client from '@libs/server/client';
-import withHandler from '@libs/server/withHandler';
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 999999) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  console.log(token);
+
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
