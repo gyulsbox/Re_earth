514ce5ba7452fd0fa9901fdd410acdb5999436fd
Hayden <hayden@tpcinternet.com>
2022-06-23 22:10:54 +0900

    refactor: refactoring streams

--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -1,17 +1,23 @@
 import type { NextPage } from "next";
-import Layout from "../../components/layouts/layout";
+import Link from "next/link";
+import FloatButton from "../../components/float-button";
+import Layout from '../../components/layouts/layout';
 
-const Stream: NextPage = () => {
+const Live: NextPage = () => {
   return (
-    <Layout title="Stream" hasTabBar>
-      <div className="py-10 px-4 divide-y-2 space-y-4">
-        {new Array(10).fill(1).map((_, i) => (
-          <div key={i} className="pt-4 px-4">
-            <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-            <h3 className="text-gray-700 text-lg mt-2">look at this!</h3>
-          </div>
+    <Layout hasTabBar title="라이브">
+      <div className=" divide-y-[1px] space-y-4">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Link key={i} href={`/live/${i}`}>
+            <a className="pt-4 block  px-4">
+              <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+              <h1 className="text-2xl mt-2 font-bold text-gray-900">
+                Galaxy S50
+              </h1>
+            </a>
+          </Link>
         ))}
-        <button className="fixed bottom-16 right-16 bg-orange-400 hover:bg-orange-300 rounded-full p-3 border-transparent text-white shadow-xl transition-colors">
+        <FloatButton href="/live/create">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -26,10 +32,9 @@ const Stream: NextPage = () => {
               d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
             ></path>
           </svg>
-        </button>
+        </FloatButton>
       </div>
     </Layout>
   );
 };
-
-export default Stream;
+export default Live;
