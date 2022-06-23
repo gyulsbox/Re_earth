ec7cd5e96dc4d23cecf76ee83fcc07d3441a2ca8
Hayden <hayden@tpcinternet.com>
2022-06-24 02:36:59 +0900

    feat: add email & sms token verificaion system

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,7 @@ import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import smtpTransport from "@libs/server/email";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -11,8 +12,10 @@ async function handler(
 ) {
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : email ? { email } : null;
-  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 999999) + "";
+  
+  if (!user) return res.status(400).json({ ok: false });
+  
   const token = await client.token.create({
     data: {
       payload,
@@ -29,6 +32,7 @@ async function handler(
       },
     },
   });
+
   if (phone) {
     const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
@@ -36,6 +40,27 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Re:Earth Verification Code ",
+      text: `Hello! Your Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      },
+    );
+    smtpTransport.close();
+    console.log(result);
   }
   return res.json({
     ok: true,
