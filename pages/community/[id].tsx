3c42d91ba9022d63b8f29b00ad40752f125129f3
Hayden <hayden@tpcinternet.com>
2022-06-27 19:04:40 +0900

    feat: post empathy ui

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import { Comment, Post, User } from "@prisma/client";
 import Link from "next/link";
+import useMutation from "@libs/client/useMutation";
+import { setClassName } from "@libs/client/utils";
 
 interface CommentWithUser extends Comment {
   user: User;
@@ -22,15 +24,35 @@ interface PostWithUser extends Post {
 interface CommunityPostResponse {
   ok: boolean;
   post: PostWithUser;
+  isEmpathy: boolean;
 }
 
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
-  const { data, error } = useSWR<CommunityPostResponse>(
+  const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null,
   );
-  console.log(data);
-
+  const [empathy] = useMutation(`/api/posts/${router.query.id}/empathy`);
+  const onEmpathyClick = () => {
+    if (!data) return;
+    mutate(
+      {
+        ...data,
+        post: {
+          ...data?.post,
+          _count: {
+            ...data?.post._count,
+            empathy: data.isEmpathy
+              ? data?.post._count.empathy - 1
+              : data?.post._count.empathy + 1,
+          },
+        },
+        isEmpathy: !data.isEmpathy,
+      },
+      false,
+    );
+    empathy({});
+  };
   return (
     <Layout canGoBack>
       <div>
@@ -41,9 +63,9 @@ const CommunityPostDetail: NextPage = () => {
           <div className="w-10 h-10 rounded-full bg-slate-300" />
           <div>
             <p className="text-sm font-medium text-gray-700">
-              {data?.post.user.name}
+              {data?.post?.user.name}
             </p>
-            <Link href={`/users/profiles/${data?.post.user.id}`}>
+            <Link href={`/users/profiles/${data?.post?.user.id}`}>
               <a className="text-xs font-medium text-gray-500">
                 View profile &rarr;
               </a>
@@ -53,10 +75,16 @@ const CommunityPostDetail: NextPage = () => {
         <div>
           <div className="mt-2 px-4 text-gray-700">
             <span className="text-orange-500 font-medium">Q.</span>{" "}
-            {data?.post.question}
+            {data?.post?.question}
           </div>
           <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full">
-            <span className="flex space-x-2 items-center text-sm">
+            <button
+              onClick={onEmpathyClick}
+              className={setClassName(
+                "flex space-x-2 items-center text-sm",
+                data?.isEmpathy ? "text-teal-400" : "",
+              )}
+            >
               <svg
                 className="w-4 h-4"
                 fill="none"
@@ -71,8 +99,8 @@ const CommunityPostDetail: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>궁금해요 {data?.post._count.empathy}</span>
-            </span>
+              <span>궁금해요 {data?.post?._count.empathy}</span>
+            </button>
             <span className="flex space-x-2 items-center text-sm">
               <svg
                 className="w-4 h-4"
@@ -88,12 +116,12 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 {data?.post._count.comments}</span>
+              <span>답변 {data?.post?._count.comments}</span>
             </span>
           </div>
         </div>
         <div className="px-4 my-5 space-y-5">
-          {data?.post.comments.map((comment) => (
+          {data?.post?.comments.map((comment) => (
             <div key={comment.id} className="flex items-start space-x-3">
               <div className="w-8 h-8 bg-slate-200 rounded-full" />
               <div>
