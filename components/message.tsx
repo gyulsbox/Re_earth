fbe9b7e6ce9e11581ead41ece088a66b4f6ac090
Hayden <hayden@tpcinternet.com>
2022-07-15 03:00:58 +0900

    feat: components layout, message, product-list modified

--- components/message.tsx
+++ components/message.tsx
@@ -1,4 +1,5 @@
 import { setClassName } from "@libs/client/utils";
+import Image from 'next/image';
 
 interface MessageProps {
   message: string;
@@ -18,7 +19,18 @@ export default function Message({
         reversed ? "flex-row-reverse space-x-reverse" : "",
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
+      {avatarUrl ? (
+        <Image
+          width={40}
+          height={40}
+          src={`https://imagedelivery.net/w46l_DmHQSMJLI8NrmR8QQ/${avatarUrl}/avatar`}
+          className="w-8 h-8 rounded-full"
+          alt="chat-avartar"
+        />
+      ) : (
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+      )}
+
       <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
         <p>{message}</p>
       </div>
