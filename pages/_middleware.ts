2c7454d73a99b56e08cb9f060f989169c5e79b0e
Hayden <hayden@tpcinternet.com>
2022-07-20 21:01:53 +0900

    chore: delete middleware log

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,8 +2,6 @@ import type { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log("it's global middleware");
-
   if (req.ua?.isBot) {
     return new Response("Please don't be a bot. Be human.", { status: 403 });
   }
