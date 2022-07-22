bc95458b0072a457510f25b371c72725f44e5dca
Hayden <hayden@tpcinternet.com>
2022-07-22 17:28:35 +0900

    fix: middleware modified cause of next update

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -2,11 +2,11 @@ import type { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.ua?.isBot) {
-    return new Response("Please don't be a bot. Be human.", { status: 403 });
-  }
+  // if (req.ua?.isBot) {
+  //   return new Response("Please don't be a bot. Be human.", { status: 403 });
+  // }
   if (!req.url.includes("/api")) {
-    if (!req.url.includes("/enter") && !req.cookies.reearthSession) {
+    if (!req.url.includes("/enter") && !req.cookies.get("reearthSession")) {
       return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
     }
   }
