0906ef8b2202aeae259e2e4ab93e4bfe5bbd0cda
Hayden <hayden@tpcinternet.com>
2022-07-22 17:46:10 +0900

    fix: middleware is modified

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,9 @@ export default function middleware(req: NextRequest, ev: NextFetchEvent) {
   // if (req.ua?.isBot) {
   //   return new Response("Please don't be a bot. Be human.", { status: 403 });
   // }
-  if (!req.url.includes("/api")) {
-    if (!req.url.includes("/enter") && !req.cookies.get("reearthSession")) {
-      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
-    }
-  }
+  // if (!req.url.includes("/api")) {
+  //   if (!req.url.includes("/enter") && !req.cookies.get("reearthSession")) {
+  //     return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+  //   }
+  // }
 }
