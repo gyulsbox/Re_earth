a4dba0ffb332c092fc40b974e24077b2615595ae
Hayden <hayden@tpcinternet.com>
2022-07-22 21:24:33 +0900

    feat: formatTime Funcion

--- libs/client/utils.ts
+++ libs/client/utils.ts
@@ -1,3 +1,17 @@
+import moment from "moment";
+
 export function setClassName(...classnames: string[]) {
-  return classnames.join(' ')
+  return classnames.join(" ");
+}
+
+export function formatTime(date: Date) {
+  const nowDate = moment();
+  const dayDiff = moment(nowDate).diff(date, "days");
+  const hourDiff = moment(nowDate).diff(date, "hours");
+  if (dayDiff <= 0) {
+    if (hourDiff <= 0) {
+      return moment(nowDate).diff(date, "minutes") + "분 전";
+    }
+    return hourDiff + "시간 전";
+  } else return dayDiff + "일 전";
 }
