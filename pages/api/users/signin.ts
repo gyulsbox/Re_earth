ed26e71bf48fd28dc97c3613d87ff6f8c73d87e4
Hayden <hayden@tpcinternet.com>
2022-07-27 03:52:02 +0900

    feat: Add signUp api

---
+++ pages/api/users/signin.ts
@@ -0,0 +1,33 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
+  const { username, password } = req.body;
+
+  if (!username || !password) return res.status(400).json({ ok: false });
+
+  const checkUser = await client.user.findFirst({
+    where: {
+      username,
+      password,
+    },
+  });
+
+  if (checkUser) {
+    req.session.user = {
+      id: checkUser.id,
+    };
+    await req.session.save();
+    console.log(checkUser);
+    return res.json({ ok: true });
+  }
+}
+
+export default withApiSession(
+  withHandler({ method: ["POST"], handler, isPrivate: false }),
+);
