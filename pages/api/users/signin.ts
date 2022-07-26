3d34fddd959a17fefc7c97527d196b3dc6fdf6ef
Hayden <hayden@tpcinternet.com>
2022-07-27 05:02:17 +0900

    fix: signin api fix

--- pages/api/users/signin.ts
+++ pages/api/users/signin.ts
@@ -9,13 +9,13 @@ async function handler(
 ) {
   const { username, password } = req.body;
 
-  if (!username || !password) return res.status(400).json({ ok: false });
-
   const checkUser = await client.user.findUnique({
     where: {
       username,
     },
   });
+
+  if (!checkUser) return res.status(404).end();
 
   if (checkUser && checkUser.password === password) {
     req.session.user = {
