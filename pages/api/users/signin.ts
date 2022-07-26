b8e75d03ee793a905d143f90e051d1280db53bc2
Hayden <hayden@tpcinternet.com>
2022-07-27 05:00:22 +0900

    fix: signin api fix

--- pages/api/users/signin.ts
+++ pages/api/users/signin.ts
@@ -3,23 +3,17 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@libs/server/withSession";
 
-interface LoginForm {
-  username: string;
-  password: string;
-}
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
-  const { username, password }: LoginForm = req.body;
+  const { username, password } = req.body;
 
   if (!username || !password) return res.status(400).json({ ok: false });
 
-  const checkUser = await client.user.findFirst({
+  const checkUser = await client.user.findUnique({
     where: {
       username,
-      password,
     },
   });
 
